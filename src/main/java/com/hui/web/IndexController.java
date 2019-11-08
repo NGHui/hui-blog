772a825d5d139aba6c21efebb5fb27168dcd4034
聂广辉 <2284129306@qq.com>
2019-11-08 08:43:20 +0800

    实现博客全文搜索的功能

--- src/main/java/com/hui/web/IndexController.java
+++ src/main/java/com/hui/web/IndexController.java
@@ -1,10 +1,8 @@
 package com.hui.web;
 
-import com.hui.NotFoundException;
 import com.hui.service.BlogService;
 import com.hui.service.TagService;
 import com.hui.service.TypeService;
-import com.hui.vo.BlogQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -12,7 +10,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by limi on 2017/10/13.
@@ -37,6 +36,15 @@ public class IndexController {
         model.addAttribute("tags", tagService.listTagTop(10));
         model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));
         return "index";
+    }
+
+
+    @PostMapping("/search")
+    public String search(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                         @RequestParam String query, Model model) {
+        model.addAttribute("page", blogService.listBlog("%"+query+"%", pageable));
+        model.addAttribute("query", query);
+        return "search";
     }
 
     @GetMapping("/blog/{id}")
